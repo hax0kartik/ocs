43ea0eadade25d30eac1e7e5dd7dcd18d48249fa
Pirater12 <pokemonemerald0@gmail.com>
2017-09-11 22:54:50 +0530

    Add a gm9 script download and skip downloading files if they already exist in step 1

--- include/fs.h
+++ include/fs.h
@@ -5,3 +5,5 @@ void fsOpenAndWrite(const char *location, void *data, size_t size);
 Result fsOpenAndWriteNAND(const char *location, void *data, size_t size);
 
 u8 *fsOpenAndRead(const char *location, u32 *readSize);
+
+Result checkFileExists(const char *location);
